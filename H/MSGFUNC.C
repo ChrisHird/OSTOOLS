41b39e393877b74620fefa812984d6314c0b0488
chrish <chrish@debian-omen.shield.local>
2018-03-13 14:19:00 -0400

    Remove line numbers

--- H/MSGFUNC.C
+++ H/MSGFUNC.C
@@ -1,18 +1,18 @@
-000100180313#ifndef MSGFUNC_h
-000101180313   #define MSGFUNC_h
-000102180313   #include <qusec.h>                          // Error Code Structs
-000103180313   #include <qmhsndm.h>                        // snd msg
-000104180313
-000105180313   //the default message queue
-000106180313   #define _DFT_MSGQ "OSMSGQ    *LIBL     "
-000107180313   // error code structure with 1KB message data
-000108180313   typedef _Packed struct  Os_EC_x {
-000109180313                           Qus_EC_t EC;
-000110180313                           char Exception_Data[1024];
-000111180313                           } Os_EC_t;
-000112180313   // define size
-000113180313   #define _ERR_REC sizeof(struct Os_EC_x);
-000114180313
-000115180313   // function declarations
-000116180313   void snd_error_msg(Os_EC_t);
-000117180313   #endif
+#ifndef MSGFUNC_h
+   #define MSGFUNC_h
+   #include <qusec.h>                          // Error Code Structs
+   #include <qmhsndm.h>                        // snd msg
+
+   //the default message queue
+   #define _DFT_MSGQ "OSMSGQ    *LIBL     "
+   // error code structure with 1KB message data
+   typedef _Packed struct  Os_EC_x {
+                           Qus_EC_t EC;
+                           char Exception_Data[1024];
+                           } Os_EC_t;
+   // define size
+   #define _ERR_REC sizeof(struct Os_EC_x);
+
+   // function declarations
+   void snd_error_msg(Os_EC_t);
+   #endif
--- QCSRC/MSGFUNC.C
+++ QCSRC/MSGFUNC.C
@@ -1,54 +1,54 @@
-000101180313//
-000102180313// Copyright (c) 2018 Chris Hird
-000103180313// All rights reserved.
-000104180313//
-000105180313// Redistribution and use in source and binary forms, with or without
-000106180313// modification, are permitted provided that the following conditions
-000107180313// are met:
-000108180313// 1. Redistributions of source code must retain the above copyright
-000109180313//    notice, this list of conditions and the following disclaimer.
-000110180313// 2. Redistributions in binary form must reproduce the above copyright
-000111180313//    notice, this list of conditions and the following disclaimer in the
-000112180313//    documentation and/or other materials provided with the distribution.
-000113180313//
-000114180313// Disclaimer :
-000115180313// This code is distributed in the hope that it will be useful,
-000116180313// but WITHOUT ANY WARRANTY; without even the implied warranty of
-000117180313// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-000118180313
-000119180313#include <H/MSGFUNC>                        // MsgFunc header
-000120180313
-000124180313// function snd_error_msg()
-000125180313// Purpose: Forward an Error Message to the message queue.
-000126180313// @parms
-000127180313//      Error Code Structure
-000128180313// returns void
-000129180313
-000130180313void snd_error_msg(Os_EC_t Error_Code) {
-000131180313int data_len = 0;
-000132180313char Msg_Type[10] = "*INFO     ";           // msg type
-000133180313char Msg_File[20] = "QCPFMSG   *LIBL     "; // Message file to use
-000134180313char Msg_Key[4] = {' '};                    // msg key
-000135180313char QRpy_Q[20] = {' '};                    // reply queue
-000136180313Os_EC_t E_Code = {0};                       // error code struct
-000137180313
-000138180313Error_Code.EC.Bytes_Provided = _ERR_REC;
-000139180313data_len = Error_Code.EC.Bytes_Available - 16;  // BA + BP +msgid +rsvd
-000140180313QMHSNDM(Error_Code.EC.Exception_Id,
-000141180313        Msg_File,
-000142180313        Error_Code.Exception_Data,
-000143180313        data_len,
-000144180313        Msg_Type,
-000145180313        _DFT_MSGQ,
-000146180313        1,
-000147180313        QRpy_Q,
-000148180313        Msg_Key,
-000149180313        &E_Code);
-000150180313if(E_Code.EC.Bytes_Available > 0) {
-000151180313   // if we get an error on sending the message send it
-000152180313   snd_error_msg(E_Code);
-000153180313   }
-000154180313return;
-000155180313}
-000156180313
-000157180313
+//
+// Copyright (c) 2018 Chris Hird
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// Disclaimer :
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+#include <H/MSGFUNC>                        // MsgFunc header
+
+// function snd_error_msg()
+// Purpose: Forward an Error Message to the message queue.
+// @parms
+//      Error Code Structure
+// returns void
+
+void snd_error_msg(Os_EC_t Error_Code) {
+int data_len = 0;
+char Msg_Type[10] = "*INFO     ";           // msg type
+char Msg_File[20] = "QCPFMSG   *LIBL     "; // Message file to use
+char Msg_Key[4] = {' '};                    // msg key
+char QRpy_Q[20] = {' '};                    // reply queue
+Os_EC_t E_Code = {0};                       // error code struct
+
+Error_Code.EC.Bytes_Provided = _ERR_REC;
+data_len = Error_Code.EC.Bytes_Available - 16;  // BA + BP +msgid +rsvd
+QMHSNDM(Error_Code.EC.Exception_Id,
+        Msg_File,
+        Error_Code.Exception_Data,
+        data_len,
+        Msg_Type,
+        _DFT_MSGQ,
+        1,
+        QRpy_Q,
+        Msg_Key,
+        &E_Code);
+if(E_Code.EC.Bytes_Available > 0) {
+   // if we get an error on sending the message send it
+   snd_error_msg(E_Code);
+   }
+return;
+}
+
+
